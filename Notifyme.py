ea44573f83b0ebb4e72553c801679872764a43e9
Rodio346 <vvyy346@gmail.com>
2021-12-22 22:30:29 +0530

    Notify me

---
+++ Notifyme.py
@@ -0,0 +1,15 @@
+from win10toast import ToastNotifier
+import time
+toaster = ToastNotifier()
+
+header = input("What You Want Me To Remember\n")
+text = input("Releated Message\n")
+time_min=float(input("In how many minutes?\n"))
+
+time_min = time_min * 60
+print("Setting up reminder..")
+time.sleep(2)
+print("all set!")
+time.sleep(time_min)
+toaster.show_toast(f"{header}", f"{text}", duration=10, threaded=True)
+while toaster.notification_active(): time.sleep(0.005)  
---
+++ getbackhere.py
@@ -0,0 +1,5 @@
+# To sole purpose of this script is to get you back to work if you are being distracted.
+# I suffer from this a lot and i know some you you guy to might be going with the same. 
+# So for now 
+# First iteration : best method is to add an alarm that rings in every 30minutes or so and if you fail to close that in 10 secs it will take it as you were not working 
+#                   so we are going to start with it and keep improving then. 
